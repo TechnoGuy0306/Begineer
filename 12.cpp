8e1fea3157f5908755ff139e3dcd43f5393c935d
Shivamkg <33519092+Shivamkg@users.noreply.github.com>
2018-08-07 11:48:42 +0530

    my first commit

---
+++ 12.cpp
@@ -0,0 +1,50 @@
+#include<ios.stream>
+using namespacestd;
+int main()
+{
+	cout<< "choice";
+	cin>> "ch";
+	switch "ch";
+	
+	{
+		case1:
+			printlist()	;
+			break;
+	    case2:
+		    insert(s,m);
+			break;
+		case3:
+		    delete(s,m);
+			break;
+		case4:
+		    exit();
+	}
+	
+	
+  void printlist( char s*[], int*a )
+  {
+  	for (int i=0; i<*a; i++)
+  	{
+  		cout<< *s[i];
+  		*(s+i);
+  		
+	}
+  }
+	void insertname (char *s[]' int *a)
+	{
+		int pos;
+		char name[20];
+		cout<< "enter position at which name is to be found";
+		cin>> pos;
+		cout<< "enter name";
+		cin>> name;
+	
+	
+	void deletename	
+	}
+	
+	
+	
+	
+}
+
